fc8cf0c3a1aafaefac28f887b557320d8bbc725f
clozicomunica <fernanda@clozicomunica.com>
2025-08-11 17:05:38 -0300

    update on analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { CartProvider } from "./context/CartProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CartProvider>
       <App />
+      <Analytics />
     </CartProvider>
   </StrictMode>
 );
