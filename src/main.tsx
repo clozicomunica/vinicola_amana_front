a83943a86d1e2ecb45fb2dff94fdc3e8326b4428
clozicomunica <fernanda@clozicomunica.com>
2025-08-11 23:02:34 -0300

    update on speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { CartProvider } from "./context/CartProvider";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
